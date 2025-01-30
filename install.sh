cfe66c4647d5c5aaa89fcb9255d5517537e4e1fa
Nakildias <130524745+Nakildias@users.noreply.github.com>
2025-01-30 16:11:50 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Create Directory
+mkdir ~/.config/BashSSH
+#Move Config to ~/.config/BashSSH/
+mv ./BashSSH.config ~/.config/BashSSH/
+#Making BashSSH Executable
+chmod +x ./BashSSH
+#Moving Script to /bin/
+sudo mv ./BashSSH /bin/
+#Making non capital alias
+sudo cp /bin/BashSSH /bin/bashssh
+echo "Done you may now use the command "BashSSH or bashssh"
